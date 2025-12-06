d994b5e63c6f74fb13ee0acfc1401fe4af53c911
Khawla 0ufir <k.oufir4283@uca.ac.ma>
2025-12-07 00:42:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const galleryItems = document.querySelectorAll('.gallery-item img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.querySelector('.lightbox-img');
+const closeBtn = document.querySelector('.close');
+
+galleryItems.forEach(item => {
+    item.addEventListener('click', () => {
+        lightbox.style.display = 'flex';
+        lightboxImg.src = item.src;
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
+lightbox.addEventListener('click', (e) => {
+    if(e.target !== lightboxImg && e.target !== closeBtn){
+        lightbox.style.display = 'none';
+    }
+});
